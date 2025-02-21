85014a7ac60ac8c9ab9becaf27b4ce7cf8ffb77a
Jesús Mendoza Verduzco <loginlock22@gmail.com>
2025-02-21 17:25:48 -0600

    Fix: search input focused onclick and onmouseenter

--- src/modules/shared/sections/SearchFloat/index.tsx
+++ src/modules/shared/sections/SearchFloat/index.tsx
@@ -1,5 +1,6 @@
 import searchIcon from '@/assets/search.svg';
 import styles from './SearchFloat.module.scss';
+import { useRef } from 'react';
 
 type Props = {
   onChange?: (value: string) => void;
@@ -7,10 +8,12 @@ type Props = {
 };
 
 function SearchFloat({ onChange, value }: Props) {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onMouseEnter={() => inputRef.current?.focus()} onClick={() => inputRef.current?.focus()}>
       <img src={searchIcon} alt="Search" />
       <input
+        ref={inputRef}
         onChange={(e) => onChange?.(e.target.value)}
         value={value}
         type="text"
